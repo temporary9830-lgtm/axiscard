6ee4085ad2b698db7eefb9d459e936bc038cbf52
Debraj Halder <debrajh13@gmail.com>
2026-08-27 23:00:38 +0530

    Fix database setup

--- newproject/wsgi.py
+++ newproject/wsgi.py
@@ -4,4 +4,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newproject.settings')
 
 application = get_wsgi_application()
-app = application
+app = application  # Vercel-এর জন্য এটি যোগ করা জরুরি
