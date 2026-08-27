fa1a65e03f15b9e18a8c464c4fc6d82270b23a0d
Debraj Halder <debrajh13@gmail.com>
2026-08-27 17:38:26 +0530

    Add build_files.sh script

--- newproject/settings.py
+++ newproject/settings.py
@@ -104,7 +104,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'staticfiles',
 ]
 
 # Production static assets location for collectstatic
