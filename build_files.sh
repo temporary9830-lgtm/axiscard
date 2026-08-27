d86050c072356eff0139c574d25d1824f7607ca0
Debraj Halder <debrajh13@gmail.com>
2026-08-27 17:14:52 +0530

    Add build_files.sh script

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,7 @@
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
+
+# Django Settings Path বলে দেওয়া
+export DJANGO_SETTINGS_MODULE=newproject.settings
+
 python3 manage.py collectstatic --noinput
