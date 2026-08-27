074ff2cbe01b70212db11f7e8f3dafde9322028d
Debraj Halder <debrajh13@gmail.com>
2026-08-27 22:53:46 +0530

    Fix database setup

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,6 @@
 python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --noinput
+#!/bin/bash
+pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate --noinput
